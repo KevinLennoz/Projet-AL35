56f6e04741b7273a39b875b87cadcf7cccf7c6cd
MathildaLandreau <mathilda.landreau@wanadoo.fr>
2021-05-25 14:29:42 +0200

    Commit cart avec total euros

--- Favorite/src/main/java/fr/eql/al35/service/CartIService.java
+++ Favorite/src/main/java/fr/eql/al35/service/CartIService.java
@@ -1,9 +1,12 @@
 package fr.eql.al35.service;
 
 import fr.eql.al35.entity.Cart;
+import fr.eql.al35.entity.CommandArticle;
 
 public interface CartIService {
 	
 	int getCartProductsQuantity(Cart cart);
+	double getTotalPriceCart(Cart cart);
+	int addProduct(CommandArticle commandArticle);
 
 }

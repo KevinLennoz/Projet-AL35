3c2995c1ca43f06453569a12e088ac644fd6f102
DuvCharles <charles.duvigneau@gmail.com>
2021-05-25 18:45:27 +0200

    Product + Cart controller

--- Favorite/src/main/java/fr/eql/al35/service/CartIService.java
+++ Favorite/src/main/java/fr/eql/al35/service/CartIService.java
@@ -2,11 +2,14 @@ package fr.eql.al35.service;
 
 import fr.eql.al35.entity.Cart;
 import fr.eql.al35.entity.CommandArticle;
+import fr.eql.al35.entity.Product;
+
 
 public interface CartIService {
 	
 	int getCartProductsQuantity(Cart cart);
 	double getTotalPriceCart(Cart cart);
-	int addProduct(CommandArticle commandArticle);
+	public boolean enoughInStock(CommandArticle commandArticle, Product product);
+	void addArticle(Cart cart, CommandArticle commandArticle);
 
 }

9d934c15ab1e6d2a751bf4411abc334cbb7c341c
MathildaLandreau <mathilda.landreau@wanadoo.fr>
2021-05-26 17:03:18 +0200

    Commit fin merge CommandService

--- Favorite/src/main/java/fr/eql/al35/service/CommandService.java
+++ Favorite/src/main/java/fr/eql/al35/service/CommandService.java
@@ -8,27 +8,40 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.eql.al35.entity.Article;
 import fr.eql.al35.entity.Cart;
 import fr.eql.al35.entity.Command;
+import fr.eql.al35.entity.Custom;
+import fr.eql.al35.entity.Stock;
 import fr.eql.al35.entity.Vat;
+
 import fr.eql.al35.repository.AddressIRepository;
 import fr.eql.al35.repository.CityIRepository;
+
+import fr.eql.al35.repository.ArticleIRepository;
+
 import fr.eql.al35.repository.CommandIRepository;
+
 import fr.eql.al35.repository.PayModeIRepository;
+
+import fr.eql.al35.repository.CustomIRepository;
+import fr.eql.al35.repository.SizeIRepository;
+
 import fr.eql.al35.repository.StatusIRepository;
+import fr.eql.al35.repository.StockIRepository;
 import fr.eql.al35.repository.UserIRepository;
 import fr.eql.al35.repository.VatIRepository;
 
 @Service
 @Transactional
 public class CommandService implements CommandIService {
-	
+
 	@Autowired
 	CommandIRepository cmdRepo;
-	
+
 	@Autowired
 	StatusIRepository statusRepo;
-	
+
 	@Autowired
 	VatIRepository vatRepo;
 	
@@ -42,8 +55,18 @@ public class CommandService implements CommandIService {
 	PayModeIRepository payModeRepo;
 	
 	// plus besoin de ces repos quand le front sera finit : 
+
+	@Autowired
+	ArticleIRepository articleRepo;
+
+	@Autowired
+	CustomIRepository customRepo;
+
 	@Autowired
 	UserIRepository userRepo;
+	
+	@Autowired
+	StockIRepository stockRepo;
 
 	@Override
 	public Command createCommand(Cart cart) {
@@ -52,7 +75,7 @@ public class CommandService implements CommandIService {
 		command.setTaxOutPrice(cart.getPrice());
 		return command;
 	}
-	
+
 	@Override
 	public Command saveCommand(Command command) {
 		Vat vat = vatRepo.findById(5).get(); //en dur global pour la command, a modifier pour chaque article plus tard
@@ -60,9 +83,16 @@ public class CommandService implements CommandIService {
 		command.setTaxInPrice(command.getTaxOutPrice() + command.getTaxOutPrice()*vat.getRate());
 		command.setCreationDate(LocalDateTime.now());
 		command.setStatus(statusRepo.findById(1).get());
+		articleRepo.saveAll(command.getArticles());
 		cmdRepo.save(command);
+		for (Article article : command.getArticles()) {
+			article.setCommand(command);
+			updateStock(article);
+		}
+		articleRepo.saveAll(command.getArticles());
 		return command;
 	}
+
 
 	@Override
 	public List<Command> displayAllCommands() {
@@ -82,5 +112,10 @@ public class CommandService implements CommandIService {
 	}
 
 
-	
+	private void updateStock(Article article) {
+		Stock stock = stockRepo.findStockByProductAndSize(article.getProduct(), article.getSize());
+		stock.setQuantity(stock.getQuantity() - article.getQuantity());
+		stockRepo.save(stock);
+	}
+
 }
